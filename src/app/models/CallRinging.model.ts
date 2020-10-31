78be65804cdbf14002d82e5a33dcf0004af67523
Samuel Hübner <samu.huebner@gmail.com>
2020-10-31 15:37:38 +0100

    - Updates model files

---
+++ src/app/models/CallRinging.model.ts
@@ -0,0 +1,9 @@
+import { CallDestination } from './CallDestination.model';
+
+export interface CallRinging { 
+    id: number;
+    callId: string;
+    callRingingTime: Date;
+    destinationId: number;
+    callDestination: CallDestination;
+}
