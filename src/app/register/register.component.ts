4f93bc3e213e7a1d53be3c0d5781aa8e01af7a50
Samuel Hübner <samu.huebner@gmail.com>
2021-07-28 20:46:02 +0200

    refactor(*): Changes the styles to reflect the new look

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../models/User.model';
 import { AuthService } from '../services/auth/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoadingIndicatorService } from '../services/loading-indicator/loading-indicator.service';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
     hide = true;
 
-    constructor(private authService: AuthService, private router: Router, private snackBar: MatSnackBar) {}
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private snackBar: MatSnackBar,
+        private loadingIndicatorService: LoadingIndicatorService,
+    ) {}
 
-    ngOnInit(): void {}
-
-    onSubmit(user: User) {
+    onSubmit(user: User): void {
+        this.loadingIndicatorService.show();
         this.authService
             .createUser(user)
             .then(() => {
+                this.loadingIndicatorService.hide();
                 this.router.navigate(['login']);
                 this.snackBar.open('Please verify your account with the email we sent you', 'Okay', { duration: 3000 });
             })
@@ -32,6 +38,7 @@ export class RegisterComponent implements OnInit {
                 } else {
                     this.snackBar.open('Unknown error during registering', 'Okay', { duration: 3000 });
                 }
+                this.loadingIndicatorService.hide();
             });
     }
 }
